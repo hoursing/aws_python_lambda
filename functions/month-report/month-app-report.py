d3e1b270d830aa1698e7c44577efbafd188d8193
hoursing.com <hoursing.com@gmail.com>
2023-04-25 13:21:46 +0700

    add deploy layer

--- functions/month-report/month-app-report.py
+++ functions/month-report/month-app-report.py
@@ -1,8 +1,5 @@
 import sys
-from demo.test_layer import Test
 
-test = Test("Hello world MONTH report")
 
 def handler(event, context):
-    test.getName()
     return 'Xin chao from AWS Lambda using Python' + sys.version + '!'
