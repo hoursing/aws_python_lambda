3cf11146d335a0daa225017857b493d008377b88
hoursing.com <hoursing.com@gmail.com>
2023-04-25 14:05:15 +0700

    fix deploy

--- functions/day-report/day-app-report.py
+++ functions/day-report/day-app-report.py
@@ -1,8 +1,12 @@
+import os
 import sys
 from demo.test_layer import Test
+from demo.test_ssm import SsmTest
 
 test = Test("Hello world DAY Report")
+ssmTest = SsmTest()
 
 def day_handler(event, context):
     test.getName()
+    ssmTest.get_by_dict(os.getenv('TEST_LIST_PARAMETER'))
     return 'Xin chao Day report from AWS Lambda using Python' + sys.version + '!'
