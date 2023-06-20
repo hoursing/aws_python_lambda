c2808a07b6e292176506a4780269283bb74723af
hoursing.com <hoursing.com@gmail.com>
2023-06-20 09:39:30 +0700

    Fix date

--- layers/test-utils/layer/demo/test_2.py
+++ layers/test-utils/layer/demo/test_2.py
@@ -1,10 +1,14 @@
 class Test2:
-    def __init__(self, name, title) -> None:
+    def __init__(self, name, title, date) -> None:
         self.name = name
         self.title = title
+        self.date = date
 
     def getName(self):
         print(self.name)
 
     def getTitle(self):
         print(self.title)
+
+    def getDate(self):
+        print(self.date)
